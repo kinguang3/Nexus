b5f7cbbb8df331fe387667a164d945fb3f7e9df2
User <user@example.com>
2026-06-10 19:52:47 +0800

    Update Test_API_KEY.py

--- Test/Test_API_KEY.py
+++ Test/Test_API_KEY.py
@@ -12,7 +12,7 @@ completion = client.chat.completions.create(
     model="qwen3.7-plus",  # 您可以按需更换为其它深度思考模型
     messages=messages,
     extra_body={"enable_thinking": True},
-    stream=True
+    stream=True# 开启流式输出
 )
 is_answering = False  # 是否进入回复阶段
 print("\n" + "=" * 20 + "思考过程" + "=" * 20)
@@ -25,4 +25,4 @@ for chunk in completion:
         if not is_answering:
             print("\n" + "=" * 20 + "完整回复" + "=" * 20)
             is_answering = True
-        print(delta.content, end="", flush=True)
+        print(delta.content, end="", flush=True)#flash的作用是刷新缓冲区，确保立即显示内容
