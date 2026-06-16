aaef515d6c8db7282805d0ec60d8a6818d09ad09
User <user@example.com>
2026-06-16 19:31:23 +0800

    Update Test_API_KEY.py

--- Test/Test_API_KEY.py
+++ Test/Test_API_KEY.py
@@ -7,13 +7,14 @@ client = OpenAI(
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
-messages = [{"role": "user", "content": "你是谁"}]
+messages = [{"role": "user", "content": "你是谁"}]#相当于添加提示词
 completion = client.chat.completions.create(
     model="qwen3.7-plus",  # 您可以按需更换为其它深度思考模型
-    messages=messages,
+    messages=messages,# 输入消息列表，后面可以添加自己的消息
     extra_body={"enable_thinking": True},
     stream=True# 开启流式输出
 )
+# 初始化对话状态
 is_answering = False  # 是否进入回复阶段
 print("\n" + "=" * 20 + "思考过程" + "=" * 20)
 for chunk in completion:
