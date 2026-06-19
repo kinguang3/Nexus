0ebf0e1998dd32ebc34d61a6a3eaeabaec5cf99a
User <user@example.com>
2026-06-19 15:10:22 +0800

    Update cosine_similarity.py and langchain_test.py

--- Test/cosine_similarity.py
+++ Test/cosine_similarity.py
@@ -8,7 +8,7 @@ def get_mag(a):# 计算向量a的模长
     return (sum(a[i] ** 2 for i in range(len(a)))) ** 0.5
 
 
-def get_cosine(a, b):# 计算向量a和b的余弦相似度
+def get_cosine(a, b):# 计算向量a和b的余弦相似度(就是向量a和b的夹角的余弦值)
     return get_dot(a, b) / (get_mag(a) * get_mag(b))
 
 
--- Test/langchain_test.py
+++ Test/langchain_test.py
@@ -1,22 +1,22 @@
 from langchain_openai import ChatOpenAI
 from langchain_community.embeddings.dashscope import DashScopeEmbeddings
 
-llm = ChatOpenAI(
+llm = ChatOpenAI(# 初始化ChatOpenAI模型，聊天模型
     model="qwen-max",
     api_key="sk-ee1dd984f89a4569bed597e2cb0eaace",
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
 )
 
-embeddings = DashScopeEmbeddings(
-    model="text-embedding-v3",
+embeddings = DashScopeEmbeddings(# 初始化DashScopeEmbeddings模型，嵌入模型
+    model="text-embedding-v3",# 模型名称
     dashscope_api_key="sk-ee1dd984f89a4569bed597e2cb0eaace"
 )
 print(embeddings.embed_query("你是谁"))
 
-messages = [
+messages = [#chatopenai的messages参数
     {"role": "system", "content": "你是一个专业的助手"},
     {"role": "user", "content": "你是谁"}
 ]
 
-for chunk in llm.stream(messages):
+for chunk in llm.stream(messages):# 流式输出
     print(chunk.content, end="", flush=True)
